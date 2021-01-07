a741da3e90e155e10b8215f141468734b4afcebf
Muhammad <www.sajidrajput666@gmail.com>
2021-01-07 13:48:39 +0500

    addGhPages

--- src/screens/HomePage/index.js
+++ src/screens/HomePage/index.js
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
-import React from 'react';
-=======
+
+import React from 'react'
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 
->>>>>>> df2534ab76bd5fd9bc9bb2e023cbfffed472ef14
+
 
 
 const HomePage = () => {
 
-<<<<<<< HEAD
-   return (
-    
-       <div >
-     <h1> Home PAge </h1>
-     </div>
-   )
-     
-}
-
-=======
   return (
     <div>
     <h1>homepage</h1>
@@ -33,5 +21,4 @@ const HomePage = () => {
 }
 
 
->>>>>>> df2534ab76bd5fd9bc9bb2e023cbfffed472ef14
 export default HomePage;
